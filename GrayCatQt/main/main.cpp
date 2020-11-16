82774d7497604c187ccf3322feae6702a15ecdee
zzp <greycatya@163.com>
2020-11-16 19:55:10 +0800

    test

--- GrayCatQt/main/main.cpp
+++ GrayCatQt/main/main.cpp
@@ -7,14 +7,22 @@ int main(int argc, char *argv[])
     // [0] 进程单例 - 不可重复打开
     SingleApplication a(argc, argv);
     CATLOG::CatLog::Instance();
-
-    CATLOG::CatLog::__Write_Log(DEBUG_LOG("test"));
-
     std::thread th([](){
         std::thread::id tid = std::this_thread::get_id();
         std::cout << "main id=" << tid << std::endl;
         std::cout.flush();
     });
     th.join();
+
+
+    std::thread ths([](){
+        std::thread::id tid = std::this_thread::get_id();
+        std::cout << "main id=" << tid << std::endl;
+        std::cout.flush();
+    });
+    ths.join();
+    CATLOG::CatLog::__Write_Log(DEBUG_LOG("test"));
+
+
     return a.exec();
 }

565be11905959c11a5864659f68a87a655cb7959
“ubuntuyw” <test>
2020-12-11 19:37:48 +0800

    更新滚动条

--- GrayCatQt/Src/CatControl/CatSlider.cpp
+++ GrayCatQt/Src/CatControl/CatSlider.cpp
@@ -1,58 +1,6 @@
-﻿#include "CatSlider.h"
-#include <QPushButton>
+#include "CatSlider.h"
 
-CatSlider::CatSlider(QWidget *parent)
-    : QWidget(parent)
-{
-    InitUi();
-    InitProperty();
-    InitConnect();
-}
-
-CatSlider::~CatSlider()
-{
-
-}
-
-void CatSlider::InitUi()
-{
-    m_pSliderBackGround = new QWidget(this);
-    m_pSliderMoveBackGround = new QWidget(this);
-    m_pHeadSliding = new QPushButton(this);
-    m_pTailSliding = new QPushButton(this);
-}
-
-void CatSlider::InitProperty()
-{
-
-}
-
-void CatSlider::InitConnect()
-{
-
-}
-
-void CatSlider::resizeEvent(QResizeEvent *event)
-{
-
-}
-
-void CatSlider::showEvent(QShowEvent *event)
-{
-
-}
-
-void CatSlider::mouseMoveEvent(QMouseEvent *event)
-{
-
-}
-
-void CatSlider::mousePressEvent(QMouseEvent *event)
-{
-
-}
-
-void CatSlider::mouseReleaseEvent(QMouseEvent *event)
+CatSlider::CatSlider()
 {
 
 }
--- GrayCatQt/Src/CatControl/CatSlider.h
+++ GrayCatQt/Src/CatControl/CatSlider.h
@@ -1,42 +1,12 @@
-﻿#ifndef CATSLIDER_H
+#ifndef CATSLIDER_H
 #define CATSLIDER_H
 
-#include <QWidget>
 
-class QPushButton;
-
-class CatSlider : public QWidget
+class CatSlider
 {
     Q_OBJECT
 public:
-    enum State {
-        TwoSlidingBlock = 2,
-        OneSlidingBlock = 1,
-        NotSlidingBlock = 0
-    };
-    explicit CatSlider(QWidget *parent = nullptr);
-    ~CatSlider();
-
-private:
-    void InitUi();
-    void InitProperty();
-    void InitConnect();
-
-protected:
-    void resizeEvent(QResizeEvent *event) override;
-    void showEvent(QShowEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-
-private:
-    Qt::Orientation m_yOrientation;
-
-    QWidget *m_pSliderBackGround;       // 滑动条背景
-    QWidget *m_pSliderMoveBackGround;   // 滑动条移动背景
-    QPushButton *m_pHeadSliding;        // 滑块 - 头部
-    QPushButton *m_pTailSliding;        // 滑块 - 尾部
-
+    CatSlider();
 };
 
 #endif // CATSLIDER_H

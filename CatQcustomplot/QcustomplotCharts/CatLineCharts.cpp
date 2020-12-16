6b8cbc6934db03766f0e6ee0900c415568cebbce
Your Name <destiny>
2020-12-17 00:41:33 +0800

    添加Qcustomplot

---
+++ CatQcustomplot/QcustomplotCharts/CatLineCharts.cpp
@@ -0,0 +1,6 @@
+#include "CatLineCharts.h"
+
+CatLineCharts::CatLineCharts()
+{
+
+}
---
+++ CatQcustomplot/QcustomplotCharts/CatLineCharts.h
@@ -0,0 +1,12 @@
+#ifndef CATLINECHARTS_H
+#define CATLINECHARTS_H
+
+
+class CatLineCharts
+{
+    Q_OBJECT
+public:
+    CatLineCharts();
+};
+
+#endif // CATLINECHARTS_H

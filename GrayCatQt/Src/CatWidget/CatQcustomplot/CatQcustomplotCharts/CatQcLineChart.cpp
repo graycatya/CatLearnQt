8215b9f58958bcdf5463474c7e651800ce30152d
“ubuntuyw” <test>
2020-12-22 20:03:38 +0800

    更新图表游标

---
+++ GrayCatQt/Src/CatWidget/CatQcustomplot/CatQcustomplotCharts/CatQcLineChart.cpp
@@ -0,0 +1,6 @@
+#include "CatQcLineChart.h"
+
+CatQcLineChart::CatQcLineChart()
+{
+
+}
---
+++ GrayCatQt/Src/CatWidget/CatQcustomplot/CatQcustomplotCharts/CatQcLineChart.h
@@ -0,0 +1,12 @@
+#ifndef CATQCLINECHART_H
+#define CATQCLINECHART_H
+
+
+class CatQcLineChart
+{
+    Q_OBJECT
+public:
+    CatQcLineChart();
+};
+
+#endif // CATQCLINECHART_H

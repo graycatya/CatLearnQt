e1b73a94f190ddb692ad41fb39a7b8e1fe51cdd8
“ubuntuyw” <test>
2020-12-23 17:33:24 +0800

    更新国际化

---
+++ GrayCatQt/Resource/lang/language_en.ts
@@ -0,0 +1,328 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>ButtonTool</name>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/ButtonTool.cpp" line="58"/>
+        <source>cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/ButtonTool.cpp" line="69"/>
+        <source>save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/ButtonTool.cpp" line="80"/>
+        <source>close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/ButtonTool.cpp" line="128"/>
+        <source>CloseLamp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/ButtonTool.cpp" line="130"/>
+        <source>OpenLamp</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatAbout</name>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="97"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="154"/>
+        <source>AppName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="164"/>
+        <source>AppVersion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="212"/>
+        <source>----------------</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="254"/>
+        <source>Author:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="261"/>
+        <source>GrayCatYa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="356"/>
+        <source>Licence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="395"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;SimSun&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Ubuntu&apos;; font-size:11pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatAbout.ui" line="410"/>
+        <source>Context</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatDrawingBoard</name>
+    <message>
+        <location filename="../../main/CatDrawingBoard.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatLineChart</name>
+    <message>
+        <location filename="../../main/WidgetTools/QcustomplotCharts/CatLineChart.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WidgetTools/QcustomplotCharts/CatLineChart.ui" line="62"/>
+        <source>ScatterShape:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WidgetTools/QcustomplotCharts/CatLineChart.ui" line="69"/>
+        <source>LineStyle:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WidgetTools/QcustomplotCharts/CatLineChart.ui" line="79"/>
+        <source>TracerStyle:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WidgetTools/QcustomplotCharts/CatLineChart.ui" line="89"/>
+        <source>PositionType:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WidgetTools/QcustomplotCharts/CatLineChart.ui" line="129"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WidgetTools/QcustomplotCharts/CatLineChart.cpp" line="63"/>
+        <source>DxVa:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatQcustomplotTools</name>
+    <message>
+        <location filename="../../main/WidgetTools/CatQcustomplotTools.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatQuickWidget</name>
+    <message>
+        <location filename="../../main/CatQuickWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatSettings</name>
+    <message>
+        <location filename="../../main/CatSettings.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatSettings.ui" line="135"/>
+        <source>Window Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/CatSettings.ui" line="162"/>
+        <source>Style:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatSliders</name>
+    <message>
+        <location filename="../qml/CatSliders.qml" line="73"/>
+        <source>Slider</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/CatSliders.qml" line="142"/>
+        <source>RangeSlider</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatWidget</name>
+    <message>
+        <location filename="../../main/CatWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GifButtonWidget</name>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/GifButtonWidget.ui" line="26"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/GifButtonWidget.ui" line="134"/>
+        <source>X:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/GifButtonWidget.ui" line="164"/>
+        <source>Y:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/GifButtonWidget.ui" line="194"/>
+        <source>W:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/GifButtonWidget.ui" line="224"/>
+        <source>H:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/GifButtonWidget.ui" line="264"/>
+        <source>Fps:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/GifButtonWidget.ui" line="302"/>
+        <source>0   帧</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageTools</name>
+    <message>
+        <location filename="../../main/WidgetTools/ImageTools.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QrenCodeTool</name>
+    <message>
+        <location filename="../../main/WidgetTools/QrenCodeTool.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuickQrenCode</name>
+    <message>
+        <location filename="../../main/QuickWidgetTools/QuickQrenCode.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuickSliders</name>
+    <message>
+        <location filename="../../main/QuickWidgetTools/QuickSliders.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RimlessWindowTool</name>
+    <message>
+        <location filename="../../main/WidgetTools/RimlessWindowTool.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScreenShot</name>
+    <message>
+        <location filename="../../Src/CatWidget/ImageTools/ScreenShot.cpp" line="429"/>
+        <location filename="../../Src/CatWidget/ImageTools/ScreenShot.cpp" line="436"/>
+        <source>( %1 , %2 ) - ( %3 x %4 )</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SliderTools</name>
+    <message>
+        <location filename="../../main/WidgetTools/SliderTools.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WidgetTools/SliderTools.ui" line="78"/>
+        <source>QSlider</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WinWidget</name>
+    <message>
+        <location filename="../../main/WinWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WinWidget.ui" line="99"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WinWidget.cpp" line="212"/>
+        <source>QWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WinWidget.cpp" line="213"/>
+        <source>QGraphicsView</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WinWidget.cpp" line="214"/>
+        <source>QQuickWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WinWidget.cpp" line="215"/>
+        <source>Setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main/WinWidget.cpp" line="216"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

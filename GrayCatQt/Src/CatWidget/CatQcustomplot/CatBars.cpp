d2fa25800b8a33764f1fbf6eb5fb4c5ed228b883
“ubuntuyw” <test>
2021-01-21 19:00:41 +0800

    更新图表项

---
+++ GrayCatQt/Src/CatWidget/CatQcustomplot/CatBars.cpp
@@ -0,0 +1,6 @@
+#include "CatBars.h"
+
+CatBars::CatBars()
+{
+
+}
---
+++ GrayCatQt/Src/CatWidget/CatQcustomplot/CatBars.h
@@ -0,0 +1,12 @@
+#ifndef CATBARS_H
+#define CATBARS_H
+
+
+class CatBars
+{
+    Q_OBJECT
+public:
+    CatBars();
+};
+
+#endif // CATBARS_H

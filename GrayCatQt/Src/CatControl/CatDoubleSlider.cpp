01f6d43ac540bbdb0128c4ecb412164777bc2040
“ubuntuyw” <test>
2021-01-27 20:14:35 +0800

    更新双滑块

---
+++ GrayCatQt/Src/CatControl/CatDoubleSlider.cpp
@@ -0,0 +1,6 @@
+#include "CatDoubleSlider.h"
+
+CatDoubleSlider::CatDoubleSlider()
+{
+
+}
---
+++ GrayCatQt/Src/CatControl/CatDoubleSlider.h
@@ -0,0 +1,13 @@
+#ifndef CATDOUBLESLIDER_H
+#define CATDOUBLESLIDER_H
+
+#include <QWidget>
+
+class CatDoubleSlider
+{
+    Q_OBJECT
+public:
+    CatDoubleSlider();
+};
+
+#endif // CATDOUBLESLIDER_H

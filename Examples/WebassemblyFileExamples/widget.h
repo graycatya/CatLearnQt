4517cdce9569a24ae69101b5b644914c9f329c34
Your Name <destiny>
2021-03-10 08:09:07 +0800

    test

---
+++ Examples/WebassemblyFileExamples/widget.h
@@ -0,0 +1,27 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_OpenFileButton_clicked();
+
+    void on_SaveFileButton_clicked();
+
+private:
+    Ui::Widget *ui;
+    QImage *image;
+};
+#endif // WIDGET_H

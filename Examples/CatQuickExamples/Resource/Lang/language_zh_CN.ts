eac765adc708f12d67b7c82164776737de019776
zzp <greycatya@163.com>
2021-06-24 18:32:24 +0800

    更新Qml源码并更新CatQuickExamples界面

---
+++ Examples/CatQuickExamples/Resource/Lang/language_zh_CN.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-24 11:52:05
+ * @LastEditTime: 2021-06-24 11:52:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \CatLearnQt\Examples\CatQuickExamples\Resource\Lang\language_zh_CN.ts
+ */

a64ffdebdae65b0bae898875891ba53d47624e92
Your Name <destiny>
2022-03-09 00:00:51 +0800

    test

--- Examples/CatWidgetExamples/WidgetTools/CatTableViewTools/CatTableModel.cpp
+++ Examples/CatWidgetExamples/WidgetTools/CatTableViewTools/CatTableModel.cpp
@@ -14,6 +14,13 @@ CatTableModel::~CatTableModel()
 
 void CatTableModel::sort(int column, Qt::SortOrder order)
 {
-    qDebug() << column << " : " << order;
+    //qDebug() << column << " : " << order;
+    if(column == 3)
+    {
+        for(int i = 1; i < rowCount(); i++)
+        {
+            qDebug() << item(i, column)->data(Qt::UserRole).toString();
+        }
+    }
     QStandardItemModel::sort(column, order);
 }
